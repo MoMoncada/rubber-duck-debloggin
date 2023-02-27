54963952ea65efef1e14cb49e25dce935375c5b3
MoMoncada <116573325+MoMoncada@users.noreply.github.com>
2023-02-27 17:12:27 +0800

    created associatios for db

---
+++ models/index.js
@@ -0,0 +1,40 @@
+//--- Importing Models ---//
+const Post = require('./Post');
+const User = require('./User');
+const Comment = require('./Comment');
+
+//--- Associations: User ---//
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+
+//--- Associations: Post ---//
+Post.belongsto(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+
+//--- Associations: Comment ---//
+Comment.belongsTo(User,{
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(Post,{
+    foreignKey: 'post_id',
+    onDelete: 'SET NULL'
+});
+
+
+module.exports = {User, Post, Comment};
