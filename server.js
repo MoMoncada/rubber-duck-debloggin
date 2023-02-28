1c7781a283ebaacc6f973a4fa439e305a2461240
MoMoncada <116573325+MoMoncada@users.noreply.github.com>
2023-03-01 00:25:43 +0800

    Created server.js, commented out modules and functions we can't use yet, created auth.js and credentials .env file

--- server.js
+++ server.js
@@ -1,16 +1,51 @@
+//-- Importing packages and modules --//
+const path = require('path');
 const express = require('express');
-// import sequelize connection
+const session = require('express-session');
+const exphbs = require('express-handlebars');
 
+//-- Initializing the server --//
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+//-- Setting up Sessions --//
+const sequelize = require("./config/connection");
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+// const sess = {
+//     secret: 'Super secret secret',
+//     cookie: {
+//       maxAge:1000 * 60 * 5,
+//       httpOnly: true,
+//       secure: false,
+//       sameSite: 'strict'
+//     },
+//     resave: false,
+//     saveUninitialized: true,
+//     store: new SequelizeStore({
+//         db: sequelize
+//     })
+// };
 
+// app.use(session(sess));
 
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+// const helpers = require('./utils/helpers');
+
+// const hbs = exphbs.create({ helpers });
+
+//-- Setting up the handlebars --//
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
+
+// //-- Serving static files --//
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+//-- Routing --//
+// app.use(require('./controllers/'));
+
+//-- Starting the Server --//
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
 });
-
