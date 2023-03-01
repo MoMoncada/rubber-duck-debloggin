af7a151cd0a5f78d5262115d23fc1163b9315977
MoMoncada <116573325+MoMoncada@users.noreply.github.com>
2023-03-01 20:28:00 +0800

    added GET req for all posts

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,124 +2,55 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
+const withAuth = require("../utils/auth");
 
 //--- GET req for all posts for homepage ---//
-router.get('/', (req, res) => {
-    console.log('=====================');
-    Post.findAll({
-        attributes: [
-            'id',
-            'title',
-            'description'
-            // 'created_at'
-        ],
+router.get("/", async (req, res) => {
+    try {
+      // Get all posts and JOIN with user data
+      const dbPostData = await Post.findAll({
         include: [
-            {
-                model: Comment,
-                attributes: [
-                    'id',
-                    'description',
-                    'post_id',
-                    'user_id'
-                    // 'created_at'
-                ],
-                include: {
-                    model: User,
-                    attributes: ['username']
-                }
-            },
-            {
-                model: User,
-                attributes: ['username']
-            }
-        ]
-    })
-    .then(dbPostData => {
-        const posts = dbPostData.map(post => post.get({ plain: true }));
-        console.log(posts);
-        console.log('Im alive!');
-        res.render('homepage', {
-            posts,
-            loggedIn: req.session.loggedIn
-        });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
-
-}); 
-
-
-//-- GET req for a single post --//
-router.get('/post/:id', (req, res) => {
-    Post.findOne({
-        where: {
-            id: req.params.id
-        },
-        attributes: [
-            'id',
-            'title',
-            'description'
-            // 'created_at'
+          {
+            model: User,
+            attributes: ["username"],
+          },
         ],
-        include: [
-            {
-                model: Comment,
-                attributes: [
-                    'id',
-                    'description', 
-                    'post_id', 
-                    'user_id' 
-                    // 'created_at'
-                ],
-                include: {
-                    model: User,
-                    attributes: ['username']
-                }
-            },
-            {
-                model: User,
-                attributes: ['username']
-            }
-        ]
-    })
-    .then(dbPostData => {
-        if (!dbPostData) {
-            res.status(404).json({ message: 'No post found with this id' });
-            return;
-        }
-
-        const post = dbPostData.get({ plain: true });
-
-        res.render('single-post', {
-            post,
-            loggedIn: req.session.loggedIn,
-
-        });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
-});
-
-
-router.get('/login', (req, res) => {
-    if (req.session.loggedIn) {
-        res.redirect('/');
-        return;
+      });
+  
+      // Serialize data so the template can read it
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+  
+      // Pass serialized data and session flag into template
+      res.render("homepage", {
+        posts,
+        logged_in: req.session.logged_in,
+      });
+    } catch (err) {
+      res.status(500).json(err);
     }
+  });
 
-    res.render('login');
-});
 
-router.get('/signup', (req, res) => {
-    if (!req.session.loggedIn) {
-        res.render('signup');
-    } else {
-        res.redirect('/');
-    }
-});
+
+// //-- GET req for a single post --//
+
+
+
+// router.get('/login', (req, res) => {
+//     if (req.session.loggedIn) {
+//         res.redirect('/');
+//         return;
+//     }
+
+//     res.render('login');
+// });
+
+// router.get('/signup', (req, res) => {
+//     if (!req.session.loggedIn) {
+//         res.render('signup');
+//     } else {
+//         res.redirect('/');
+//     }
+// });
 
 module.exports = router;
